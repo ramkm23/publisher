3da9bbb4dbc866bb1a34c8bb55a7fb074add3928
Ram <creativerampm@gmail.com>
2024-10-15 16:03:25 +0530

    Changes in menu list

--- template-parts/menu-list.php
+++ template-parts/menu-list.php
@@ -2,20 +2,41 @@
 <ul class="nav-menu">
     <li class="menu-active"><a href="<?php echo get_home_url(); ?>">Home</a></li>
     <li><a href="<?php echo get_home_url(); ?>/ablout">About Us</a></li>
-    <li class="menu-has-children"><a href="">For Authors</a>
-    <ul>
-        <li><a href="<?php echo get_home_url(); ?>/pricing">Pricing</a></li>
-        <li><a href="book-format">Book Format</a></li>
-        <li><a href="<?php echo get_home_url(); ?>/book-royality">Book Royality</a></li>
-    </ul>
+    <li class="menu-has-children"><a href="<?php echo get_home_url(); ?>/authors ">For Authors</a>
+        <ul>
+            <li><a href="<?php echo get_home_url(); ?>/pricing">Pricing</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/book-format">Book Format</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/book-royality">Book Royality</a></li>
+        </ul>
     </li>
     <li class="menu-has-children"><a href="#">Published Book</a>
-    <ul>
-        <li><a href="<?php echo get_home_url(); ?>/educational-books">Educational Books</a></li>
-        <li><a href="<?php echo get_home_url(); ?>/conference-proceedings">Conference Proceedings</a></li>
-        <li><a href="<?php echo get_home_url(); ?>/edited-books">Edited Books</a></li>
-        <li><a href="/E-Books">E-Books</a></li>
-    </ul>
+        <ul>
+            <li><a href="<?php echo get_home_url(); ?>/educational-books">Educational Books</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/conference-proceedings">Conference Proceedings</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/edited-books">Edited Books</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/ebooks">E-Books</a></li>
+        </ul>
     </li> 
+    <li class="menu-has-children"><a href="#">IBJ Journals</a>
+        <!--<ul>
+            <li><a href="<?php echo get_home_url(); ?>/educational-books">Educational Books</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/conference-proceedings">Conference Proceedings</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/edited-books">Edited Books</a></li>
+            <li><a href="<?php echo get_home_url(); ?>ebooks">E-Books</a></li>
+        </ul>-->
+    </li> 
+    <li class="menu-has-children"><a href="#">IBJ Services</a>
+        <ul>
+            <li><a href="<?php echo get_home_url(); ?>/subscription-services">Subscription Services</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/paper-publication-support">Paper Publication Support</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/publish-books">Publish Books</a></li>
+            <li><a href="<?php echo get_home_url(); ?>/thesis-writing-support">Thesis Writing Support</a></li>
+        </ul>
+    </li> 
+    
     <li><a href="?php echo get_home_url(); ?>/contact">Contact Us</a></li>
 </ul>
+
+
+
+
