2faf27e04a05b76b4bb9fafbcf7c34b869623a46
Ram <creativerampm@gmail.com>
2024-10-16 14:56:27 +0530

    about us components changes

---
+++ template-parts/text-list.php
@@ -0,0 +1,35 @@
+<?php if( get_sub_field( 'text-list-include' ) == 'Yes' ) :?>
+<!-- Start Image + Text -->
+<section id="about" class="image-link wow fadeInUp">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <?php if(!empty(get_sub_field('heading'))): ?>
+                    <h2 class="titleheading"><?php echo get_sub_field('heading'); ?></h2>
+                <?php endif; ?>
+            </div>
+                    
+            <div class="col-sm-9">
+                <?php if(!empty(get_sub_field('heading'))): ?>
+                    <h3><?php echo get_sub_field('heading'); ?></h3>
+                <?php endif; ?>
+
+                <?php if(!empty(get_sub_field('text'))): ?>
+                    <p><?php echo get_sub_field('text'); ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="col-sm-3">               
+                <?php if(!empty(get_sub_field('list-heading'))): ?>
+                    <h3> <?php echo get_sub_field('list-heading'); ?></h3>
+                <?php endif; ?> 
+                
+                <?php if(!empty(get_sub_field('list-text'))): ?>
+                    <p><?php echo get_sub_field('list-text'); ?> </p> 
+                <?php endif; ?>
+                  
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Image + Text -->
+<?php endif; ?>
