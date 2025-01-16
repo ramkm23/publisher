ae70405439bd2879492b13c684603b09e36acf57
ramkmr23 <creativerampm@gmail.com>
2025-01-16 22:45:22 +0530

    made anoter canes

---
+++ event.php
@@ -0,0 +1,42 @@
+<?php
+    /*Template Name: Event */
+    
+?>
+ 
+    <header id="header">
+        <div class="container">
+            <div id="logo" class="pull-left">
+            <h1><a href="<?php echo get_home_url(); ?>" class="scrollto"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/C%20logo.png"></a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
+            </div>
+            <nav id="nav-menu-container">
+                <?php get_template_part( 'template-parts/menu-list' );?>
+            </nav><!-- #nav-menu-container -->
+        </div>
+    </header>
+ 
+
+
+<?php wp_head(); ?>
+<!-- Get Basic Content Template -->
+<?php //get_template_part('template-parts/most-popular-news'); ?>
+<div id="content" role="main">
+	<?php while ( have_posts() ) : the_post(); ?>     
+		<?php // Get Standard Comp	onents
+			if( have_rows('basic-content') ):
+
+				while ( have_rows('basic-content') ) : the_row();
+
+					$section_path = 'template-parts/'.get_row_layout();
+
+					get_template_part($section_path);
+
+				endwhile;
+
+			endif;
+		?>	
+	<?php endwhile; // end of the loop. ?>
+	<?php //get_template_part('template-parts/full-width-blogs'); ?>
+</div><!-- #content -->
+<?php get_footer(); ?>
---
+++ template-parts/four-column-components.php
@@ -0,0 +1,45 @@
+<?php if( get_sub_field( 'include-four-column-components' ) == 'Yes' ) :?>
+<!--==========================
+   Services Section
+   ============================-->
+<section class="four-coloumn-components">
+   <div class="container">
+      <div class="row"> 
+         <div class="col-sm-12">
+               <div class="comptitle">
+                  <h2><?php if(!empty(get_sub_field('four-column-components-heading'))): ?>
+                  <span><?php echo get_sub_field('four-column-components-heading'); ?></span>
+                  <?php endif; ?>
+                  </h2>
+                  <p><?php if(!empty(get_sub_field('four-column-components-text'))): ?>
+                  <?php echo get_sub_field('four-column-components-text'); ?>
+                  <?php endif; ?></p>
+               </div>
+         </div>
+         <?php if(have_rows('four-column-components-items')):?>
+         <?php while(have_rows('four-column-components-items')): the_row(); ?>
+         <div class="col-lg-3">
+            <div class="box wow fadeInLeft">
+               <div class="image">
+                  <img src="<?php echo get_sub_field( 'four-column-components-item-image' )[ 'url' ]; ?>" class="text-images igure-img img-fluid"   alt="<?php echo get_sub_field( 'three-column-components-item-image' )[ 'alt' ]; ?>">         
+               </div>
+               <?php if(!empty(get_sub_field('four-column-components-item-heading'))): ?>
+                  <h3 class="title"><?php echo get_sub_field('four-column-components-item-heading'); ?></h3>
+               <?php endif; ?>
+               <?php if(!empty(get_sub_field('four-column-components-item-text'))): ?>
+                  <p class="description">
+                     <?php echo get_sub_field('four-column-components-item-text'); ?>
+                  </p>
+               <?php endif; ?>
+               <?php if(get_sub_field('four-column-components-item-button')) : ?>  
+                    <a href="<?php echo get_sub_field('four-column-components-item-button')['url']; ?>" target="<?php echo get_sub_field('three-column-components-item-button')['target']; ?>" class="btn"><?php echo get_sub_field('three-column-components-item-button')['title']; ?></a>
+                <?php endif; ?>
+            </div>
+         </div>
+         <?php endwhile; ?>
+         <?php endif; ?>    
+      </div>
+   </div>
+</section>
+<!-- #services -->
+<?php endif; ?>
